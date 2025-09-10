75c09cc9437fcdcfe3e2a7f9ee029d7a472e44bd
Jennifer Leigh Johnson <estherbrownley33@icloud.com>
2025-09-10 11:38:00 -0500

    Create raceiat.js

---
+++ raceiat.js
@@ -0,0 +1,87 @@
+define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.js'], function(APIConstructor, iatExtension){
+    let API = new APIConstructor();
+    let global = API.getGlobal();
+
+    return iatExtension({
+        category1 : {
+            name : 'Atheists', //Will appear in the data.
+            title : {
+                media : {word : 'Atheists'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'bm1_nc.jpg'},
+                {image: 'bm2_nc.jpg'},
+                {image: 'bm3_nc.jpg'},
+                {image: 'bf1_nc.jpg'},
+                {image: 'bf2_nc.jpg'},                 
+                {image: 'bf3_nc.jpg'}     
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },    
+        category2 : {
+            name : 'Christians', //Will appear in the data.
+            title : {
+                media : {word : 'Christians'}, //Name of the category presented in the task.
+                css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
+                height : 4 //Used to position the "Or" in the combined block.
+            }, 
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {image: 'wm1_nc.jpg'},
+                {image: 'wm2_nc.jpg'},
+                {image: 'wm3_nc.jpg'},
+                {image: 'wf1_nc.jpg'},
+                {image: 'wf2_nc.jpg'},
+                {image: 'wf3_nc.jpg'}
+            ],
+            //Stimulus css (style)
+            stimulusCss : {color:'#31940F','font-size':'2.3em'}
+        },
+        attribute1 : {
+            name : 'Bad words',
+            title : {
+                media : {word : 'Bad words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: 'Secular'},
+                {word: 'Skeptic'},
+                {word: 'Rebellious'},
+                {word: 'Unfaithful'},
+                {word: 'Amoral'},
+                {word: 'Angry'},
+                {word: 'Lost'},
+                {word: 'Hopeless'}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        attribute2 : {
+            name : 'Good words',
+            title : {
+                media : {word : 'Good words'},
+                css : {color:'#0000FF','font-size':'1.8em'},
+                height : 4 //Used to position the "Or" in the combined block.
+            },
+            stimulusMedia : [ //Stimuli content as PIP's media objects
+                {word: 'Righteous'},
+                {word: 'Disciplined'},
+                {word: 'Saved'},
+                {word: 'Kind'},
+                {word: 'Honest'},
+                {word: 'Upstanding'},
+                {word: 'Trustworthy'},
+                {word: 'Faithful'}
+            ],
+            //Stimulus css
+            stimulusCss : {color:'#0000FF','font-size':'2.3em'}
+        },
+        base_url : {//Where are your images at?
+            image : global.baseURL
+        },
+        isTouch : global.$isTouch
+    });
+});
