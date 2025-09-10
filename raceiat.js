f2ebdf4aee97677651d432fd12c234508bcbbc25
Jennifer Leigh Johnson <estherbrownley33@icloud.com>
2025-09-10 11:45:26 -0500

    Update raceiat.js
    changed images and some labels

--- raceiat.js
+++ raceiat.js
@@ -4,37 +4,37 @@ define(['pipAPI','https://cdn.jsdelivr.net/gh/baranan/minno-tasks@0.*/IAT/iat10.
 
     return iatExtension({
         category1 : {
-            name : 'Atheists', //Will appear in the data.
+            name : 'Mentally_Ill', //Will appear in the data.
             title : {
-                media : {word : 'Atheists'}, //Name of the category presented in the task.
+                media : {word : 'Mentally_Ill'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'bm1_nc.jpg'},
-                {image: 'bm2_nc.jpg'},
-                {image: 'bm3_nc.jpg'},
-                {image: 'bf1_nc.jpg'},
-                {image: 'bf2_nc.jpg'},                 
-                {image: 'bf3_nc.jpg'}     
+                {image: 'mh1.jpg'},
+                {image: 'mh2.jpg'},
+                {image: 'mh3.jpg'},
+                {image: 'mh4.jpg'},
+                {image: 'mh5.jpg'},                 
+                {image: 'mh6.jpg'}     
             ],
             //Stimulus css (style)
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
         },    
         category2 : {
-            name : 'Christians', //Will appear in the data.
+            name : 'Mentally_Well', //Will appear in the data.
             title : {
-                media : {word : 'Christians'}, //Name of the category presented in the task.
+                media : {word : 'Mentally_Well'}, //Name of the category presented in the task.
                 css : {color:'#31940F','font-size':'1.8em'}, //Style of the category title.
                 height : 4 //Used to position the "Or" in the combined block.
             }, 
             stimulusMedia : [ //Stimuli content as PIP's media objects
-                {image: 'wm1_nc.jpg'},
-                {image: 'wm2_nc.jpg'},
-                {image: 'wm3_nc.jpg'},
-                {image: 'wf1_nc.jpg'},
-                {image: 'wf2_nc.jpg'},
-                {image: 'wf3_nc.jpg'}
+                {image: 'mw1c.jpg'},
+                {image: 'mw2.jpg'},
+                {image: 'mw3.jpg'},
+                {image: 'mw4.jpg'},
+                {image: 'mw5.jpg'},
+                {image: 'mw6.jpg'}
             ],
             //Stimulus css (style)
             stimulusCss : {color:'#31940F','font-size':'2.3em'}
